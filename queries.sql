ac1d8fd3595699c0ddcf2a71a966df4c531396c0
worashf <uppert83@gmail.com>
2022-08-10 17:12:12 +0300

    Implemented delete and update inside transaction

--- queries.sql
+++ queries.sql
@@ -24,7 +24,36 @@ SELECT species FROM animals; --verify update made
 ROLLBACK;
 SELECT species FROM animals; --verify update made undone
 
+/* update  and commit start*/
+BEGIN;
+UPDATE animals SET species ='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species ='pokemon' WHERE species IS NULL;
+SELECT name,species from animals; --verify update made
+COMMIT;
+SELECT name,species from animals; --verify update made after commit
+/* update and commit end */
 
+/* delete all animals data inside transaction and undo deletion start*/
+BEGIN;
+DELETE FROM animals;
+SELECT COUNT(*) FROM animals; --check the count of animals 
+SELECT * FROM animals; -- check after deletion
+ROLLBACK;
+--check the count of animals after rollback
+SELECT COUNT(*) FROM animals; 
+/* delete all animals data inside transaction and undo deletion end*/
 
+/** delete all  animals born after jan 1st 2022; */
+BEGIN;
+DELETE FROM animals WHERE date_of_birth >='01/01/2022';
+SELECT name, date_of_birth FROM animals;
+ -- create savepoint 
 
+--create savepoint 
+SAVEPOINT sp1;
+UPDATE animals SET weight_kg =weight_kg * -1;
+SELECT name, weight_kg FROM animals;
+ ROLLBACK TO sp1;
+ SELECT name, weight_kg FROM animals; -- check after rollback
+/*  */
 /** Transaction, grouping and agregate end */
